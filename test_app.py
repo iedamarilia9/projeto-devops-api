8be26c7c337b823ad8792e03d246ff65a12453d9
Iêda Marília <158054213+iedamarilia9@users.noreply.github.com>
2025-05-04 11:44:23 -0300

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,14 @@
+def test_soma():
+    assert 1 + 1 == 2
+
+def test_subtracao():
+    assert 5 - 3 == 2
+
+def test_multiplicacao():
+    assert 2 * 3 == 6
+
+def test_divisao():
+    assert 10 / 2 == 5
+
+def test_string():
+    assert "devops".upper() == "DEVOPS"
